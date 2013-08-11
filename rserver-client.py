580f783058fb947823822f89f13031289ac52e98
Leif Johnson <leif.t.johnson@gmail.com>
2013-08-10 22:52:31 -0700

    Simplify config and command line processing.

--- rserver-client.py
+++ rserver-client.py
@@ -3,78 +3,27 @@
 # a test client for rserver 
 #
 
+import argparse
 from xmlrpclib import ServerProxy, Error
 import xml.dom.minidom 
 import sys
 
-help_str  = "--help"
-xml_str   = "--xml"
-url_str   = "--url"
-qtext_str = "--questiontext"
-var_str   = "--variables"
-ans_str   = "--answer"
-tol_str   = "--tolerance"
-aid_str   = "--ansid"
-#######################################################################
-def print_usage():
-	print sys.argv[0]
-	print "\t", url_str, "server_url"
-	print "\t[", help_str, "] print this help msg"
-	print "\t[", xml_str, "xmlfile] process an xml file of questions"
-	print "\t[", var_str, "variable_string]"
-	print "\t[", qtext_str, "questiontext_string]"
-	print "\t[", ans_str, "answer_string]"
-	print "\t[", aid_str, "answerid]"
-	print "\t[", tol_str, "tolerance_string]"
-	print "Note: While all the components are optional, invoking without any"
-	print "      is a rather silly thing to do."
-	print "Note: answer, tolerance and ansid may be repeated many times"
-	print "      each is processed in the order passed independently"
-	print "Note: an xmlfile may contain as many questions as you like, they"
-	print "      will be processed sequentially"
+parser = argparse.ArgumentParser(description='Send xml requests to an RServer.')
+parser.add_argument('--xmlfile', dest='xmlfile', type=str, nargs=1, 
+    help='process a xml file of questions.')
+parser.add_argument('--url', dest='serverurl', type=str, 
+    help='server url to send requests to.', nargs=1, required=True)
+parser.add_argument('--variables', dest='variables', type=str, nargs=1, 
+    help='variables string to send to server.')
+parser.add_argument('--questiontext', dest='questiontext', type=str, nargs=1, 
+    help='questiontext string to send to server.')
+parser.add_argument('--answer', dest='answer', type=str, nargs='+', 
+    help='answer string to send to server.')
+parser.add_argument('--answerid', dest='ansid', type=int,
+    help='answer id to send to server.', nargs='+')
+parser.add_argument('--tolerance', dest='tolerance', type=str,
+    help='tolerance string to send to server.', nargs='+')
 
-#######################################################################
-def get_question(args):
-	# we don't need to worry about even length, it will be checked before we
-	# are called
-	# blank question to return, we will fill it as we go
-	question = {'questiontext': "", 'variables': "", 'answers': []}
-	server   = ""
-	# these keep the answer components in the right place
-	ans_idx = tol_idx = ans_idx = 0
-	idx = 0
-	while(idx < len(args)):
-		option = args[idx]
-		value  = args[idx+1]
-		if option == url_str: 
-			server = value
-		elif option == qtext_str: 
-			question['questiontext'] = value
-		elif option == var_str: 
-			question['variables'] = value
-		elif option == ans_str or option == tol_str or option == aid_str:
-			# these are a little more complicated
-			if option == ans_str: a_idx = ans_idx
-			elif option == tol_str: a_idx = tol_idx
-			else: a_idx = aid_idx
-			while(len(question['answers']) < (a_idx + 1)):
-				question['answers'].append({'ansid': 0, 'answer': "", 'tolerance': ""})
-			if option == ans_str:
-				question['answers'][ans_idx]['answer'] = value
-				ans_idx = ans_idx + 1
-			if option == tol_str:
-				question['answers'][tol_idx]['tolerance'] = value
-				tol_idx = tol_idx + 1
-			if option == aid_str:
-				question['answers'][aid_idx]['ansid'] = value
-				aid_idx = aid_idx + 1
-		else:
-			## unknown option
-			print option, "is not recognized"
-			print_usage()
-			sys.exit(1)
-		idx = idx + 2
-	return (server, question)
 #######################################################################
 def send_question(url, question):
 	server = ServerProxy(url)
@@ -93,8 +42,8 @@ def process_questions(server, questions):
 def convert_question_toxml(question):
 	doc = xml.dom.minidom.Document()
 	qxml = doc.createElement("question")
-	qxml.setAttribute("type", "ltjprocesed")
-	for key in ['name', 'variables', 'questiontext', 'numanswers']:
+	qxml.setAttribute("type", "remoteprocessed")
+	for key in ['name','variables', 'questiontext', 'numanswers']:
 		if question.has_key(key):
 			ml = doc.createElement(key)
 			ml.appendChild(doc.createTextNode(str(question[key])))
@@ -113,70 +62,51 @@ def convert_question_toxml(question):
 	return qxml
 #######################################################################
 def process_command_line(args):
-	questions    = []
-	question     = {}
+	parsed_args = parser.parse_args(args)
+	args_dict = vars(parsed_args)
+	args_dict = dict((k, v) for k, v in args_dict.items() if v is not None)
+	args_keys = set(args_dict.keys()).difference(set("serverurl"))
+	if 'xmlfile' in args_dict and len(args_dict) > 1:
+		print "You may not specifiy xmlfile with any other options"
+		sys.exit(0)
+	return args_dict
+
+
+def read_xml_options(args):
+	question = dict()
+	for key in ['questiontext', 'variables']:
+		if key in args:
+			question[key] = args[key][0]
+
+	# Goal: build the repeatable answer fields into a list of dictionaries.
+	#       What comes in is args['ansid'] = ansid_list, 
+	#       args['answer'] = answer_list, and 
+	#       args['tolerance'] = tolerance_list.  We want to convert this to
+	#       a list of dictionaries, having the form:
+	#         [{'answer': answer_list[0], 'tolerance': tolerance_list[0], 
+	#           'ansid': ansid_list[0]}, ...]
+	#       There is probably a better python way of doing this.
+	# Find the longest of the repeatable answer fields.
 	question['answers'] = []
-	server       = ""
-	xml_file     = ""
-	
-	xml_found    = False
-	cl_found     = False
-	idx = 0
-	ans_idx = 0
-	tol_idx = 0
-	aid_idx = 0
-	while(idx < len(args)):
-		option = args[idx]
-		value  = args[idx+1]
-		if option == url_str:
-			server = value
-		elif option == xml_str:
-			if xml_file != "":
-				print "Can only handle 1 xml file at a time"
-				sys.exit(1)
-			xml_file = value
-			xml_found = True
-		elif option == qtext_str: 
-			question['questiontext'] = value
-			cl_found = True
-		elif option == var_str: 
-			question['variables'] = value
-			cl_found = True
-		elif option == ans_str or option == tol_str or option == aid_str:
-			cl_found = True
-			# these are a little more complicated
-			if option == ans_str: a_idx = ans_idx
-			elif option == tol_str: a_idx = tol_idx
-			else: a_idx = aid_idx
-			while(len(question['answers']) < (a_idx + 1)):
-				question['answers'].append({'ansid': 0, 'answer': "", 'tolerance': ""})
-			if option == ans_str:
-				question['answers'][ans_idx]['answer'] = value
-				ans_idx = ans_idx + 1
-			if option == tol_str:
-				question['answers'][tol_idx]['tolerance'] = value
-				tol_idx = tol_idx + 1
-			if option == aid_str:
-				question['answers'][aid_idx]['ansid'] = value
-				aid_idx = aid_idx + 1
-		else:
-			## unknown option
-			print option, "is not recognized"
-			print_usage()
-			sys.exit(1)
-		idx = idx + 2
-			
+	answer_keys = [
+		x for x in ['ansid', 'answer', 'tolerance'] if x in args.keys()]
+	if len(answer_keys) == 0:
+		return [question]
 
-	if xml_found and cl_found:
-		print "Can't mix command line and xml questions"
-		sys.exit(1)
+	answer_dict_lists = [[{key: v} for v in args[key]] for key in answer_keys]
+	numanswers = max(len(l) for l in answer_dict_lists)
 
-	if xml_found:
-		questions = read_xml_file(xml_file)
-	elif cl_found:
-		questions.append(question)
-	
-	return process_questions(server, questions)
+	for i in xrange(len(answer_dict_lists)):
+		for j in xrange(len(answer_dict_lists[i]), numanswers):
+			answer_dict_lists[i].append(dict())
+
+	for i in xrange(numanswers):
+		question['answers'].append(dict())
+		for j in xrange(len(answer_dict_lists)):
+			question['answers'][i].update(answer_dict_lists[j][i])
+
+	return [question]
+
 #######################################################################
 def read_xml_file(xml_file):
 	questions = []
@@ -184,7 +114,7 @@ def read_xml_file(xml_file):
 	rootnode = xmldata.childNodes[0]
 	for node in rootnode.childNodes:
 		if node.localName == 'question':
-			if node.getAttribute("type") == "ltjprocessed":
+			if node.getAttribute("type") == "remoteprocessed":
 				questions.append(process_qnode(node))
 	return questions
 				
@@ -193,17 +123,17 @@ def read_xml_file(xml_file):
 ## this xml indexing seems a little obtuse, this probably means I'm doing it wrong
 def process_qnode(qnode):
 	question = {}
-	ltjnode = None
+	remote_node = None
 	for node in qnode.childNodes:
 		if node.localName == "name":
 			question['name'] = node.childNodes[1].childNodes[0].nodeValue
 		elif node.localName == "questiontext":
 			question['questiontext'] = node.childNodes[1].childNodes[0].nodeValue
-		elif node.localName == "ltjprocessed":
-			ltjnode = node
+		elif node.localName == "remoteprocessed":
+			remote_node = node
 	ansnd = None
-	if ltjnode != None:
-		for node in ltjnode.childNodes:
+	if remote_node != None:
+		for node in remote_node.childNodes:
 			if node.localName == 'variables':
 				question['variables'] = node.childNodes[0].nodeValue
 			elif node.localName == 'answers':
@@ -212,31 +142,18 @@ def process_qnode(qnode):
 	answers = []
 	for nd in ansnd.childNodes:
 		if nd.localName == 'answer':
-			ans = {}
-			for nd1 in nd.childNodes:
-				if nd1.localName != None:
-					ans[nd1.localName] = nd1.childNodes[0].nodeValue
+			ans = dict((nd1.localeName, nd1.childNodes[0].nodeValue) for
+				nd1 in nd.childNodes if nd1.localName is not None)
 			answers.append(ans)
 	question['answers'] = answers
 	return question
 				
 	
 #######################################################################
-if len(sys.argv) < 2:
-	# this is no good
-	print "Need command line options!"
-	print_usage()
-	sys.exit(1)
-
-if len(sys.argv[1:]) == 1 and sys.argv[1] == help_str:
-	print_usage()
-	sys.exit(0)
-	
-if len(sys.argv[1:]) % 2 != 0:
-	print "Need an even number of command line options!"
-	print_usage()
-	sys.exit(1)
-
-process_command_line(sys.argv[1:])
-
-sys.exit(0)
+if __name__ == "__main__":
+	command_options = process_command_line(sys.argv[1:])
+	if 'xmlfile' in command_options:
+		questionxml_list = read_xml_file(command_options['xmlfile'][0])
+	else:
+		questionxml_list = read_xml_options(command_options)
+	process_questions(command_options['serverurl'][0], questionxml_list)
--- rserver.py
+++ rserver.py
@@ -8,6 +8,8 @@ sys.setdefaultencoding('utf-8')
 
 import argparse
 import base64
+import ConfigParser
+import io
 import os
 import SimpleXMLRPCServer
 import time
@@ -15,37 +17,46 @@ import time
 ################################################################################
 # Define command line arguments.
 parser = argparse.ArgumentParser(description='Launch an RServer process.')
-parser.add_argument('--tmpdir', dest='tmpdir', type=str, nargs='?', 
-                    help='temporary directory.', default='/tmp')
-parser.add_argument('--logfile', dest='logfile', type=argparse.FileType('w'), 
-                    help='logfile.', default='rserver.log', nargs='?')
-parser.add_argument('--host', dest='host', type=str, nargs='?', 
-                    help='hostname to serve.', default='localhost')
-parser.add_argument('--port', dest='port' type=int, nargs='?', 
-                    help='port to serve from.', default=8080)
-parser.add_argument('--loglevel', dest='loglevel', type=int, nargs='?', 
-                    help='logging level.', default=1)
+parser.add_argument('--tmpdir', dest='tmpdir', type=str, nargs=1, 
+    help='temporary directory.')
+parser.add_argument('--logfile', dest='logfile', type=str, 
+    help='logfile.', nargs=1)
+parser.add_argument('--host', dest='host', type=str, nargs=1, 
+    help='hostname to serve.')
+parser.add_argument('--port', dest='port', type=int, nargs=1, 
+    help='port to serve from.')
+parser.add_argument('--loglevel', dest='loglevel', type=int, nargs=1, 
+    help='logging level.')
+parser.add_argument('--config', dest='config', type=str,
+    help='configuration file.', nargs=1)
+parser.add_argument('--clean_r', dest='clean_r', type=str,
+    help='R expressions to run to generate a clean R environment.', nargs='*')
+
+config_values = {'tmpdir': '/tmp',
+                 'logfile': 'rserver.log',
+                 'host': 'localhost',
+                 'port': '8080',
+                 'loglevel': 1}
 
 ################################################################################
-tmpdir = "/tmp"
 
 workspacetime = str(int(time.time())%1000)
 
 def R_clean_workspace(): 
-    return tmpdir + "/" + workspacetime + ".rda"
+    return config_values['tmpdir'] + "/" + workspacetime + ".rda"
 
-def build_clean_R(): 
+def build_clean_r(): 
     return ["rm(list=ls(all=TRUE))", "load(\""+R_clean_workspace()+"\")"]
 
-def build_close_R(): 
+def build_close_r(): 
     return ["rm(list=ls(all=TRUE))"]
 
-def build_grade_R(): 
+def build_grade_r(): 
     return ['library(grade, lib.loc="/home/ruser/R-library")']
 
-clean_R = build_clean_R()
-close_R = build_close_R()
-grade_R = build_grade_R()
+config_values['clean_r'] = build_clean_r()
+config_values['close_r'] = build_close_r()
+config_values['grade_r'] = build_grade_r()
 ################################################################################
 ## this is my attempt to capture the R output, rpy made this easier
 ################################################################################
@@ -54,9 +65,9 @@ requests   = 0
 
 ## logging faclities
 def log(msg, level=2):
-    if level > loglevel:
+    if level > config_values['loglevel']:
         return
-    with open(logfile, "a") as logf:
+    with open(config_values['logfile'], "a") as logf:
         logf.write(msg)
 
 def log_question_parameter_string(question, key):
@@ -80,12 +91,12 @@ def log_question(question):
 def junk_output(s):
     return
 
-def get_clean_R():
-    for s in clean_R: r(s)
+def get_clean_r():
+    for s in config_values['clean_r']: r(s)
     return r
 
-def close_R_con(R): ## procedure to clean up and close the given R connection
-    for s in close_R: R(s)
+def close_r_con(R): ## procedure to clean up and close the given R connection
+    for s in config_values['close_r']: R(s)
     return None
 
 def get_r_output(R, rcode):
@@ -97,7 +108,7 @@ def get_r_output(R, rcode):
 
 def load_encoded_workspace(R, workspace):
     ## tmp file name
-    tmp_file = tmpdir + "/" + "rwork" + str(int(time.time())) + ".Rdata"
+    tmp_file = config_values['tmpdir'] + "/" + "rwork" + str(int(time.time())) + ".Rdata"
     fileout = open(tmp_file, "w")
     fileout.write(base64.b64decode(workspace))
     fileout.close()
@@ -115,7 +126,7 @@ def get_image(R, imgcode):
     ## as of R 2.7, png images work by default, so that's what we use here.
     ## it makes life much simpler
     ## first get a unique file name for R
-    imgfile = tmpdir + "/" + "rimg" + str(int(time.time()))
+    imgfile = config_values['tmpdir'] + "/" + "rimg" + str(int(time.time()))
 
     ## generate the image file by running the image code
     R("png(\"" + imgfile +".png\")")
@@ -129,7 +140,8 @@ def get_image(R, imgcode):
 
 def get_R_workspace(R):
     ## tmpfile name
-    workspace_tmp = tmpdir + "/" + "rwksp" + str(int(time.time())) + ".rda"
+    workspace_tmp = config_values['tmpdir'] + "/" + "rwksp" + \
+        str(int(time.time())) + ".rda"
     R("save.image(file=\"" + workspace_tmp + "\")")
     dataout = get_base64_file(workspace_tmp)
     os.remove(workspace_tmp)
@@ -175,11 +187,11 @@ def processquestion(question):
     requests = requests + 1
     log("===============================process question\n")
     log_question(question)
-    R = get_clean_R()
+    R = get_clean_r()
     ret = question
     ## process variables statement, don't capture output
     if question.has_key('variables'):
-        ret['variables'] = get_r_output(R, question['variables'])
+        get_r_output(R, question['variables'])
     ## process image code if it exists, and capture the base 64 encoded
     ## image output
     if question.has_key('imagecode') and question['imagecode'] != "":
@@ -195,26 +207,23 @@ def processquestion(question):
     elif question.has_key('answers'):
         answers_out = []
         for answer in question['answers']:
-            ans = {}
-            if answer.has_key('answer'):
-                ans['answer'] = get_r_output(R, answer['answer'])
-            if answer.has_key('tolerance'):
-                ans['tolerance'] = get_r_output(R, answer['tolerance'])
-            if answer.has_key('ansid'):
+            ans = dict((k, get_r_output(R, answer[k])) \
+              for k in ['answer' , 'tolerance'] if k in answer)
+            if 'ansid' in answer:
                 ans['ansid'] = answer['ansid']
             answers_out.append(ans)
         ret['answers'] = answers_out
         ret['numanswers'] = len(answers_out)
     log("processed question\n") 
     log_question(ret) 
-    close_R_con(R) 
+    close_r_con(R) 
     return ret
 
 def status():
-    R = get_clean_R()
+    R = get_clean_r()
     rv   = my_ri2py(R("version"))
     nmrv = my_ri2py(R("names(version)"))
-    close_R_con(R)
+    close_r_con(R)
     rv = dict(zip(nmrv, rv))
     rv['requests'] = requests
     cur_time = time.time()
@@ -227,14 +236,14 @@ def grade(question):
     global requests
     requests = requests + 1
     log("==================================grade question\n")
-    R = get_clean_R()
+    R = get_clean_r()
     if question.has_key("workspace"):
         R = load_encoded_workspace(R, question['workspace'])
         log("received workspace\n")
         log("ls: " + get_r_output(R, "ls()") + "\n")
     ## now that we have the loaded workspace, calculate the student answer
     ## and store it in the R variable 'studentans'
-	for s in grade_R: R(s)
+	for s in config_values['grade_r']: R(s)
     if question.has_key("studentans"):
         R("studentans <- \"" + str(question['studentans']) + "\"")
         log("studentans: " + get_r_output(R, "studentans") + "\n")
@@ -248,110 +257,87 @@ def grade(question):
                 log("answer result: " + str(ans_result) + "\n")
                 if ans_result:
                     if answer.has_key('ansid'):
-                        log("answer correct, ansid: " + str(answer['ansid']) + "\n")
+                        log("answer correct, ansid: " + \
+                            str(answer['ansid']) + "\n")
                         ret.append(int(answer['ansid']))
     if len(ret) == 0:
         ret.append(0)
-    close_R_con(R)
+    close_r_con(R)
     return ret
 
 #################################################################
-def read_cmdline():
-    args = parser.parse_args()
+def load_configfile(filename):
+    # Read and parse a config file.
+    config = ConfigParser.SafeConfigParser()
+    try:
+        config.read(filename)
+    except ConfigParser.MissingSectionHeaderError:
+        # config file doesn't have a section header, so we'll make one up.
+        config_bytes = "[default]\n" + open(filename).read()
+        config.readfp(io.BytesIO(config_bytes))
+    else:
+        # The config file has other sections, we only want it to have "default".
+        # Take all of the options that are not in a "default" section and
+        # move them into default.
+        sections = config.sections()
+        if "default" not in sections:
+            config.add_section("default")
+        sections.remove("default")
+        for section in sections:
+            for key, value in config.items(section):
+                config.set("default", key, value)
+    config_dict = dict(config.items("default"))
+    # The rest of the code expects these values to be lists.
+    for key in ['grade_r', 'clean_r', 'close_r']:
+        if key in config_dict:
+            config_dict[key] = [config_dict[key]]
+    return config_dict
 
-def read_cmdline_1():
-	global tmpdir, logfile, loglevel, host, port, clean_R, close_R, grade_R
-	config_file = ""
-	opts = sys.argv[1:]
-	config_idx = 0
-	while config_idx < len(opts) and opts[config_idx] != "--config":
-		config_idx = config_idx + 1
-	if config_idx >= (len(opts)-1): 
-		return None
-	config_file = opts[config_idx + 1]
-	cfile = open(config_file, "r")
-	cfile_lines = cfile.readlines()
-	cfile.close()
-	####################################
-	# these options are to track if such and such shows up in the config file,
-	# if it is the first time for an item, we erase the array
-	clean_changed  = False
-	grade_changed  = False
-	close_changed  = False
-	tmpdir_changed = False
-	clean_f = False
-	grade_f = False
-	close_f = False
-	for l in cfile_lines:
-		eq = l.find("=")
-		p1 = l
-		p2 = ""
-		if eq != -1:
-			p1 = l[:eq]
-			p2 = l[(eq+1):]
-		def fix_string(s):
-			sharp = s.find("#")
-			if sharp != -1:
-				s = s[:sharp]
-			return s.lstrip().rstrip()
-		p1 = fix_string(p1)
-		p2 = fix_string(p2)
+def read_cmdline(args):
+    # Parse config file and command line configuration options.  Options
+    # more than one place are taken from, command line, config file, or the 
+    # defaults, in that order.
+    global config_values 
+    configured_options = dict()
+    parsed_args = dict((k, v) for k, v in vars(parser.parse_args(args)).items() 
+        if v is not None)
+    # De-list most of the args.
+    for k in parsed_args:
+        if k not in ['clean_r', 'grade_r', 'closer_r']:
+            parsed_args[k] = parsed_args[k][0]
+    if 'config' in parsed_args and parsed_args['config'] is not None:
+        configured_options.update(load_configfile(parsed_args['config'][0]))
+        del parsed_args['config']
+    configured_options.update(parsed_args)
+    config_values.update(configured_options)
+    # If build_r, clean_r, grade_r or tmpdir were specicfied, we need to update 
+    # those global values.
+    if 'tmpdir' in configured_options:
+        if 'clean_r' not in configured_options:
+            config_values['clean_r'] = build_clean_r()
+        if 'grade_r' not in configured_options:
+            config_values['grade_r'] = build_grade_r()
+        if 'close_r' not in configured_options:
+            config_values['close_r'] = build_close_r()
 
-		if p1.startswith("tmpdir"):
-			if p2 != "": 
-				tmpdir = p2
-				tmpdir_changed = True
-		elif p1.startswith("logfile"): 
-			if p2 != "": logfile = p2
-		elif p1.startswith("loglevel"): 
-			if p2 != "": loglevel = int(p2)
-		elif p1.startswith("host"): 
-			if p2 != "": host = p2
-		elif p1.startswith("port"): 
-			if p2 != "": port = int(p2)
-		elif p1.startswith("clean"): 
-			if p2 != "":
-				clean_changed = True
-				if clean_f == False:
-					clean_R = []
-					clean_f = True
-				clean_R.append(p2)
-		elif p1.startswith("close"): 
-			if p2 != "":
-				close_changed = True
-				if close_f == False:
-					close_R = []
-					close_f = True
-				close_R.append(p2)
-		elif p1.startswith("grade"): 
-			if p2 != "":
-				grade_changed = True
-				if grade_f == False:
-					grade_R = []
-					grade_f = True
-				grade_R.append(p2)
-	if tmpdir_changed:
-		if not clean_changed: clean_R = build_clean_R()
-		if not close_changed: close_R = build_close_R()
-		if not grade_changed: grade_R = build_grade_R()
 #################################################################
+def my_ri2py_inner(x):
+    if not hasattr(x, '__getitem__'):
+        return x
+    if type(x) is str:
+        return x
+    elif isinstance(x, robjects.RVector):
+        return my_ri2py(x)
+    else:
+        return x[0]
+
 def my_ri2py(obj):
 	res = robjects.default_ri2py(obj)
 	if isinstance(res, robjects.Vector):
 		if len(res) == 1:
 			res = res[0]
 		else:
-			def f(x):
-				if hasattr(x, '__getitem__'):
-					if type(x) is str:
-						return x
-					elif isinstance(x, robjects.RVector):
-						return my_ri2py(x)
-					else:
-						return x[0]
-				else:
-					return x
-			res = [f(x) for x in res]
+			res = [my_ri2py_inner(x) for x in res]
 	return res
 
 
@@ -361,18 +347,21 @@ import rpy2.robjects as robjects
 r = robjects.r
 
 if __name__ == "__main__":
-    read_cmdline()
+    read_cmdline(sys.argv[1:])
 
 	# save a 'clean' R workspace that we can return to
     r("save.image(file=\"" + R_clean_workspace() + "\")")
     print "Saved a clean workspace in %s" % R_clean_workspace()
+    print "tmpdir:", config_values['tmpdir']
+    print "logfile:", config_values['logfile']
 
     ## start the rpc server
-    server = SimpleXMLRPCServer.SimpleXMLRPCServer((host, port))
+    address = (config_values['host'], config_values['port'])
+    server = SimpleXMLRPCServer.SimpleXMLRPCServer(address)
     # register functions that can be called via xml-rpc
     server.register_function(processquestion)
     server.register_function(status)
     server.register_function(grade)
     server.register_introspection_functions()
-    print "Calling serve_forever with host: %s port: %d" % (host, port)
+    print "Calling serve_forever with host: %s port: %d" % address
     server.serve_forever()
